7abfc9d904fb897b7fbe66e2f50eb4bfe6f2fae4
rexiann <gandolkim@gmail.com>
2023-02-09 22:04:18 +0900

    [MOD] update home with daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,33 @@
 <script>
-  let number = 0;
+	let number = 0;
 
+	function increase() {
+		number = number + 1;
+	}
 
+	function decrease() {
+		number = number - 1;
+	}
+
+	function double() {
+		number = number * 2;
+	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<div>
-  {number}
+<div class="flex justify-center items-center">
+	<div class="artboard phone-1 flex flex-col justify-center items-center gap-4">
+		<input
+			bind:value={number}
+			disabled
+			type="text"
+			placeholder="Type here"
+			class="input w-full max-w-xs border-gray-400"
+		/>
+
+		<div class="btn-group">
+			<button on:click={increase} class="btn btn-active">+ 1</button>
+			<button on:click={double} class="btn">X 2</button>
+			<button on:click={decrease} class="btn">- 1</button>
+		</div>
+	</div>
 </div>
-
-<input bind:value={number} disabled />
-
-<button on:click={() => {
-  number = number + 1
-}} >
-  click 
-</button>
