c58ad583923dfeb6d6d195076f8f31e8a3a31b72
rexiann <gandolkim@gmail.com>
2023-02-14 20:39:59 +0900

    [ADD] install node-fetch

--- src/routes/menu/+page.server.ts
+++ src/routes/menu/+page.server.ts
@@ -1,32 +1,37 @@
-import { google } from "googleapis";
+import { google } from 'googleapis';
 
 /** @type {import('@sveltejs/adapter-vercel').Config} */
 export const config = {
-  envVarsInUse: ['VITE_CLIENT_EMAIL', 'VITE_PRIVATE_KEY', 'VITE_SHEET_ID']
-}
+	envVarsInUse: ['VITE_CLIENT_EMAIL', 'VITE_PRIVATE_KEY', 'VITE_SHEET_ID']
+};
 
-const BASE_URL = 'https://www.googleapis.com/auth/spreadsheets'
-const CLIENT_EMAIL = import.meta.env.VITE_CLIENT_EMAIL
-const PRIVATE_KEY = import.meta.env.VITE_PRIVATE_KEY
-const SHEET_ID = import.meta.env.VITE_SHEET_ID
-
+const BASE_URL = 'https://www.googleapis.com/auth/spreadsheets';
+const CLIENT_EMAIL = import.meta.env.VITE_CLIENT_EMAIL;
+const PRIVATE_KEY = import.meta.env.VITE_PRIVATE_KEY;
+const SHEET_ID = import.meta.env.VITE_SHEET_ID;
 
 export const load = async () => {
+	try {
+		const authorize = new google.auth.JWT(CLIENT_EMAIL, undefined, PRIVATE_KEY, [BASE_URL]);
+		console.log({ authorize });
 
-    const authorize = new google.auth.JWT(CLIENT_EMAIL, undefined, PRIVATE_KEY, [BASE_URL])
+		const googleSheet = google.sheets({
+			version: 'v4',
+			auth: authorize
+		});
+		console.log({ googleSheet });
 
-    const googleSheet = google.sheets({
-      version: 'v4',
-      auth: authorize,
-    })
+		console.log({ spreadsheetId: SHEET_ID });
+		const response = await googleSheet.spreadsheets.values.get({
+			spreadsheetId: SHEET_ID,
+			range: 'A1:G39'
+		});
+		console.log({ response });
 
-    const response = await googleSheet.spreadsheets.values.get({
-      spreadsheetId: SHEET_ID,
-      range: 'A1:G39'
-    })
+		console.log(response.data);
 
-    console.log(response.data)
-
-
-  return response.data;
-}
+		return response.data;
+	} catch (e) {
+		console.log('Error: ', e);
+	}
+};
